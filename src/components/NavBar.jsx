5509750d17ca12ff6d0d6fccafb6f70e6b5f287a
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-04-10 13:04:45 +0600

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <div className='h-[200px] bg-amber-600 py-10'>
+            <h1 className='text-5xl text-red-500'>this is navbar</h1>
+
+        </div>
+    );
+};
+
+export default NavBar;
